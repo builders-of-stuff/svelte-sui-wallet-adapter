e1999a81d2a33f2a690fd6a5d7457e14fc29864b
Kyle Truong <ktruong008@gmail.com>
2024-06-28 16:16:55 -0400

    Add shadcn components to connect modal

--- src/lib/components/connect-modal/connect-modal.svelte
+++ src/lib/components/connect-modal/connect-modal.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import * as Dialog from '$lib/components/ui/dialog/index.js';
   import { Button } from '$lib/components/ui/button/index.js';
+  import * as RadioGroup from '$lib/components/ui/radio-group/index.js';
   import type { WalletAdapter } from '$lib/wallet-adapter/wallet-adapter.type.js';
-  import {
-    getWalletUniqueIdentifier,
-    logWalletAdapterState
-  } from '$lib/wallet-adapter/wallet-adapter-tools.js';
+  import { getWalletUniqueIdentifier } from '$lib/wallet-adapter/wallet-adapter-tools.js';
 
   import SuiIcon from '../icons/sui-icon.svelte';
   import WalletListItem from './wallet-list-item.svelte';
@@ -16,10 +14,6 @@
 
   let selectedWallet = $state();
   let isDialogOpen = $state(false);
-
-  const resetSelection = () => {
-    selectedWallet = undefined;
-  };
 </script>
 
 <Dialog.Root bind:open={isDialogOpen}>
@@ -30,35 +24,35 @@
   <Dialog.Content>
     <Dialog.Header>
       <Dialog.Title>Connect</Dialog.Title>
-
-      <Dialog.Description>
-        <ul>
-          {#if walletAdapter?.wallets?.length > 0}
-            {#each walletAdapter.wallets as wallet}
-              <WalletListItem
-                name={wallet.name}
-                icon={wallet.icon}
-                isSelected={getWalletUniqueIdentifier(wallet) ===
-                  getWalletUniqueIdentifier(selectedWallet as any)}
-                onClick={() => {
-                  if (
-                    getWalletUniqueIdentifier(wallet) !==
-                    getWalletUniqueIdentifier(selectedWallet as any)
-                  ) {
-                    selectedWallet = wallet
-                    walletAdapter.connectWallet({
-                      wallet
-                    })
-                    isDialogOpen = false
-                  }
-                }}
-              />
-            {/each}
-          {:else}
-            <WalletListItem name="Sui Wallet" icon={SuiIcon} isSelected />
-          {/if}
-        </ul>
-      </Dialog.Description>
     </Dialog.Header>
+
+    <Dialog.Description>
+      {#if walletAdapter?.wallets?.length > 0}
+        <RadioGroup.Root value="" class="grid grid-cols-3 gap-4">
+          {#each walletAdapter.wallets as wallet}
+            <WalletListItem
+              name={wallet.name}
+              icon={wallet.icon}
+              isSelected={getWalletUniqueIdentifier(wallet) ===
+                    getWalletUniqueIdentifier(selectedWallet as any)}
+              onClick={() => {
+                    if (
+                      getWalletUniqueIdentifier(wallet) !==
+                      getWalletUniqueIdentifier(selectedWallet as any)
+                    ) {
+                      selectedWallet = wallet
+                      walletAdapter.connectWallet({
+                        wallet
+                      })
+                      isDialogOpen = false
+                    }
+                  }}
+            />
+          {/each}
+        </RadioGroup.Root>
+      {:else}
+        <WalletListItem name="Sui Wallet" icon={SuiIcon} isSelected />
+      {/if}
+    </Dialog.Description>
   </Dialog.Content>
 </Dialog.Root>
--- src/lib/components/connect-modal/wallet-list-item.svelte
+++ src/lib/components/connect-modal/wallet-list-item.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import * as RadioGroup from '$lib/components/ui/radio-group/index.js';
+  import { Label } from '$lib/components/ui/label/index.js';
+
   let {
     name,
     icon: Icon,
@@ -12,13 +15,18 @@
   }>();
 </script>
 
-<li>
-  <button type="button" onclick={onClick}>
-    {#if typeof Icon === 'string'}
-      <img src={Icon} alt={`${name} logo`} />
-    {:else}
-      <Icon />
-    {/if}
-    <div>{name}</div>
-  </button>
-</li>
+<Label
+  onclick={onClick}
+  for={name}
+  class={`flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground ${
+    isSelected ? 'border-primary' : ''
+  }`}
+>
+  <RadioGroup.Item value={name} id={name} class="sr-only" aria-label={name} />
+  {#if typeof Icon === 'string'}
+    <img src={Icon} alt={`${name} logo`} />
+  {:else}
+    <Icon />
+  {/if}
+  <div>{name}</div>
+</Label>
